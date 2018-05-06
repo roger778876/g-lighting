8ee7817ae81697fce66a08b51de68d8f106e28e6
roger778876 <rogercesar778876@gmail.com>
2018-05-06 17:47:42 -0400

    f

--- gmath.py
+++ gmath.py
@@ -10,43 +10,43 @@ SPECULAR_EXP = 4
 
 #lighting functions
 def get_lighting(normal, view, ambient, light, areflect, dreflect, sreflect ):
-    pass
+  pass
 
 def calculate_ambient(alight, areflect):
-    pass
+  pass
 
 def calculate_diffuse(light, dreflect, normal):
-    pass
+  pass
 
 def calculate_specular(light, sreflect, view, normal):
-    pass
+  pass
 
 def limit_color(color):
-    pass
+  pass
 
 #vector functions
 def normalize(vector):
-    pass
+  pass
 
 def dot_product(a, b):
-    pass
+  pass
 
 def calculate_normal(polygons, i):
 
-    A = [0, 0, 0]
-    B = [0, 0, 0]
-    N = [0, 0, 0]
+  A = [0, 0, 0]
+  B = [0, 0, 0]
+  N = [0, 0, 0]
 
-    A[0] = polygons[i+1][0] - polygons[i][0]
-    A[1] = polygons[i+1][1] - polygons[i][1]
-    A[2] = polygons[i+1][2] - polygons[i][2]
+  A[0] = polygons[i+1][0] - polygons[i][0]
+  A[1] = polygons[i+1][1] - polygons[i][1]
+  A[2] = polygons[i+1][2] - polygons[i][2]
 
-    B[0] = polygons[i+2][0] - polygons[i][0]
-    B[1] = polygons[i+2][1] - polygons[i][1]
-    B[2] = polygons[i+2][2] - polygons[i][2]
+  B[0] = polygons[i+2][0] - polygons[i][0]
+  B[1] = polygons[i+2][1] - polygons[i][1]
+  B[2] = polygons[i+2][2] - polygons[i][2]
 
-    N[0] = A[1] * B[2] - A[2] * B[1]
-    N[1] = A[2] * B[0] - A[0] * B[2]
-    N[2] = A[0] * B[1] - A[1] * B[0]
+  N[0] = A[1] * B[2] - A[2] * B[1]
+  N[1] = A[2] * B[0] - A[0] * B[2]
+  N[2] = A[0] * B[1] - A[1] * B[0]
 
-    return N
+  return N
